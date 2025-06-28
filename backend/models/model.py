7bdd6998924da98b0c278e036e05d76073a30e7e
lhangnocode <lhang18022005@gmail.com>
2025-06-28 21:50:42 +0700

    migration db

--- backend/models/model.py
+++ backend/models/model.py
@@ -1,13 +1,15 @@
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
+# Base = declarative_base()
+
+from database import Base
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, nullable=False)
-    email = Column(String(120), unique=True, nullable=False)
-    full_name = Column(String(100))
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    full_name = Column(String)
     #* can nhac co nen de khac null cho full_name khong
