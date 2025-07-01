d9b4fb1994dbda9d4ee48521892d6aa8bc9e9dbb
Kuro-orzz <23020674@vnu.edu.vn>
2025-07-01 17:27:35 +0700

    add login, register, hash password when login and some requirement lib

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, text, TIMESTAMP
 # from sqlalchemy.ext.declarative import declarative_base
 
 # Base = declarative_base()
@@ -8,8 +8,11 @@ from backend.app.database import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
     username = Column(String, unique=True, nullable=False)
     email = Column(String, unique=True, nullable=False)
     full_name = Column(String)
     #* can nhac co nen de khac null cho full_name khong
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), 
+                        nullable=False, server_default=text('NOW()'))
