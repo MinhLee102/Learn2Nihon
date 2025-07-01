b2089ed1984aa284e2fea970653ea4f05768c15a
Kuro-orzz <23020674@vnu.edu.vn>
2025-07-01 15:37:51 +0700

    restructure backend file

--- backend/app/model.py
+++ backend/app/model.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String
 
 # Base = declarative_base()
 
-from database import Base
+from backend.app.database import Base
 
 class User(Base):
     __tablename__ = "users"
