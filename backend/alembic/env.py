f088248387266a636442c6a4dc8ca5612b4b27cb
lhangnocode <lhang18022005@gmail.com>
2025-07-02 14:59:46 +0700

    add vocabularies table, restructure folder

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -21,7 +21,8 @@ if config.config_file_name is not None:
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
-from app.models import Base
+from app.models.user import Base
+from app.models import user, vocabulary
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,17 +1,21 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
+
+from ..schemas import user
+
+from ..models import user
 from ..database import get_db
-from .. import models, schemas, utils
+from .. import utils
 
 router = APIRouter(
     tags=['login', 'register']
 )
 
-@router.post('/login', response_model=schemas.UserOut)
+@router.post('/login', response_model=user.UserOut)
 def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(
-        models.User.email == user_credentials.username).first()
+    user = db.query(user.User).filter(
+        user.User.email == user_credentials.username).first()
 
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -23,13 +27,13 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session =
     return {'Valid credential': user}   # Fix later, what should i return here:))
 
 
-@router.post('/register', status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+@router.post('/register', status_code=status.HTTP_201_CREATED, response_model=user.UserOut)
+def create_user(user: user.UserCreate, db: Session = Depends(get_db)):
     # Hash password
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
 
-    new_user = models.User(user.model_dump())
+    new_user = user.User(user.model_dump())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
